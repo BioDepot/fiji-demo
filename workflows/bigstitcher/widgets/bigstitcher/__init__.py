22b77458c5149747848e670be62e29a3eeef2c59
Hong Hung <lhhunghimself@gmail.com>
2021-12-13 01:07:41 -0800

    Added bigstitcher workflow

---
+++ workflows/bigstitcher/widgets/bigstitcher/__init__.py
@@ -0,0 +1,10 @@
+import sysconfig
+
+# Category metadata.
+
+# Category icon show in the menu
+ICON = "icon/fiji.png"
+
+# Background color for category background in menu
+# and widget icon background in workflow.
+BACKGROUND = "#d7e5f0"
